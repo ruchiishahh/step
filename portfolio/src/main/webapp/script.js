b34a57b9c85c76ed8c969c246b72f07c733db243
Ruchi <to.ruchi08@gmail.com>
2020-05-29 13:34:08 -0700

    Make Bo's comment changes to fit formal js style

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -16,8 +16,7 @@
  * Adds a random greeting to the page.
  */
 function addRandomGreeting() {
-  const greetings =
-['Fav Show: White Collar', 'Fav Song: Saturday Nights', 'Fav Color: Blue', 'Fav Sport: Basketball', 'Fav Food: Enchiladas'];
+  const greetings = ['Fav Show: White Collar', 'Fav Song: Saturday Nights', 'Fav Color: Blue', 'Fav Sport: Basketball', 'Fav Food: Enchiladas'];
 
 
   // Pick a random greeting.
@@ -29,58 +28,69 @@ function addRandomGreeting() {
 }
 
 
-var TxtType = function(el, toRotate, period) {
-        this.toRotate = toRotate;
-        this.el = el;
-        this.loopNum = 0;
-        this.period = parseInt(period, 10) || 2000;
-        this.txt = '';
-        this.tick();
-        this.isDeleting = false;
-    };
+const TxtType = function(el, toRotate, period) {
+    this.toRotate = toRotate;
+    this.el = el;
+    this.loopNum = 0;
+    this.period = parseInt(period, 10) || 2000;
+    this.txt = '';
+    this.tick();
+    this.isDeleting = false;
 
-    TxtType.prototype.tick = function() {
-        var i = this.loopNum % this.toRotate.length;
-        var fullTxt = this.toRotate[i];
+};
 
-        if (this.isDeleting) {
+/**
+ * Create the typewrite functionality.
+ */
+
+TxtType.prototype.tick = function() {
+    const i = this.loopNum % this.toRotate.length;
+    const fullTxt = this.toRotate[i];
+
+    if (this.isDeleting) {
         this.txt = fullTxt.substring(0, this.txt.length - 1);
-        } else {
+    } else {
         this.txt = fullTxt.substring(0, this.txt.length + 1);
-        }
+    }
 
-        this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
-        var that = this;
-        var delta = 200 - Math.random() * 100;
+    let that = this;
+    let delta = 200 - Math.random() * 100;
 
-        if (this.isDeleting) { delta /= 2; }
+    if (this.isDeleting) {
+        delta /= 2;
+    }
 
-        if (!this.isDeleting && this.txt === fullTxt) {
+    if (!this.isDeleting && this.txt === fullTxt) {
         delta = this.period;
         this.isDeleting = true;
-        } else if (this.isDeleting && this.txt === '') {
+    } else if (this.isDeleting && this.txt === '') {
         this.isDeleting = false;
         this.loopNum++;
         delta = 500;
-        }
+    }
 
-        setTimeout(function() {
-        that.tick();
-        }, delta);
-    };
+    setTimeout(function() {
+    that.tick();
+    }, delta);
+};
 
-    window.onload = function() {
-        var elements = document.getElementsByClassName('typewrite');
-        for (var i=0; i<elements.length; i++) {
-            var toRotate = elements[i].getAttribute('data-type');
-            var period = elements[i].getAttribute('data-period');
-            if (toRotate) {
-              new TxtType(elements[i], JSON.parse(toRotate), period);
-            }
+/**
+ * Rotates different texts for the typewrite
+ */
+
+window.onload = function() {
+    const elements = document.getElementsByClassName('typewrite');
+    for (let i=0; i<elements.length; i++) {
+        const toRotate = elements[i].getAttribute('data-type');
+        const period = elements[i].getAttribute('data-period');
+        if (toRotate) {
+            new TxtType(elements[i], JSON.parse(toRotate), period);
         }
-        var css = document.createElement("style");
-        css.type = "text/css";
-        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
-        document.body.appendChild(css);
-    };
+    }
+    const css = document.createElement("style");
+    css.type = "text/css";
+    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #000}";
+    document.body.appendChild(css);
+};
