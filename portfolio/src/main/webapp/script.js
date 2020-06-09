ebfc00c1f8dba2b149e2240542751fa30c1bb733
Ruchi <to.ruchi08@gmail.com>
2020-06-09 19:15:52 +0000

    Add Google Map to the web app

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -37,6 +37,14 @@ function getDataComment() {
     });
 }
 
+/** Creates a map and adds it to the page. */
+function createMap() {
+  const map = new google.maps.Map(
+      document.getElementById('map'),
+      {center: {lat: 37.422, lng: -122.084}, zoom: 16});
+}
+
+
 /**
  * Deletes the dataComments from the servers and reflect on DOM.
  */
