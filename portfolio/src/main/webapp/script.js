67ee5a0195529c4e5c87ea9e6055ed00d78effef
Ruchi <to.ruchi08@gmail.com>
2020-06-10 22:57:40 +0000

    Unflatten some formatting in js & delete unused files

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,17 +1,3 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 /**
  * Fetches a name from the server and adds it to the DOM.
  */
@@ -47,26 +33,41 @@ const places = [
 
 /** Creates a map that shows landmarks around Google. */
 function createMap() {
-  const map = new google.maps.Map(
-      document.getElementById('map'),
-      {center: {lat: 37.3194, lng: -122.0091}, zoom: 15});
+    const map = new google.maps.Map(
+        document.getElementById('map'),
+        {
+            center: {
+                lat: places[0][1], 
+                lng: places[0][2]
+            },
+            zoom: 15
+        });
 
     for (let i = 0; i < places.length; i++) {
         let place = places[i];
-        addLandmark(map, place[1], place[2], place[0], place[3])
+        addLandmark(map, place[1], place[2], place[0], place[3]);
     }
 
 }
 
 /** Adds a marker that shows an info window when clicked. */
 function addLandmark(map, lat, lng, title, description) {
-  const marker = new google.maps.Marker(
-      {position: {lat: lat, lng: lng}, map: map, title: title});
-
-  const infoWindow = new google.maps.InfoWindow({content: description});
-  marker.addListener('click', () => {
-    infoWindow.open(map, marker);
-  });
+    const marker = new google.maps.Marker(
+        {
+            position: {
+                lat: lat,
+                lng: lng
+            },
+            map: map,
+            title: title
+        });
+    const infoWindow = new google.maps.InfoWindow(
+        {
+            content: description
+        });
+    marker.addListener('click', () => {
+        infoWindow.open(map, marker);
+    });
 }
 
 /**
